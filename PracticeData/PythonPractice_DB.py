81d0d6b176b0bde8ef11ba144ee3c7ecca5abcfa
darababalola <42246000+darababalola@users.noreply.github.com>
2018-08-17 11:48:20 +0100

    Python Practice Data #2

--- PracticeData/PythonPractice_DB.py
+++ PracticeData/PythonPractice_DB.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[107]:
-
-
 import matplotlib.pyplot as plt;
 import numpy as np;
 import pandas as pd;
@@ -12,18 +6,17 @@ import os;
 # print(os.getcwd())
 # actualData = pd.read_csv("/Users/oibabalola/pupil_cvs_data.csv")
 
-data = np.genfromtxt('pupil_cvs_data.csv', delimiter=',', skip_header=10, skip_footer=10, names=['c1', 'c2', 'c3', 'c4', 'c5'])
-#data = np.genfromtxt('pupil_cvs_data.csv', delimiter=',', skip_header=10, skip_footer=10, names=['Item1.Centroid.X', 'Item1.Centroid.Y', 'Item1.MajorAxisLength', 'Item1.MinorAxisLength', 'Item1.Area'])
+data = np.genfromtxt('FinalPupil.csv', delimiter=',', names=['c1', 'c2'])
 
-#data2 = np.genfromtxt('gaze2.csv', delimiter=',', skip_header=10, skip_footer=10, names=['g1', 'g2'])
-
-
-x=data['c1']
-y=data['c5']
+x=data['c2']
+y=data['c1']
 
 print(x.size)
 # Plot the data
-plt.scatter(x, y, label='c1 vs c5')
+plt.plot(x, y, label='c2 vs c1')
+plt.suptitle('Change in Pupil Location with Time')
+plt.xlabel('Time')
+plt.ylabel('Pupil Centre')
 #plt.scatter(data['c1'], data2['g2'], label='c1 vs g2') - crossing data sets
 
 #xAxisData = np.array(data[c1])
@@ -31,8 +24,7 @@ plt.scatter(x, y, label='c1 vs c5')
 
 #plt.plot(xAxisData, yAxisData, label='Col1 vs col2')
 # Add a legend
-# plt.legend()
+#plt.legend()
 
 # Show the plot
 # plt.show()
-
