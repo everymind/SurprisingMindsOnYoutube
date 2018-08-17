28791c6bb53af1b8cb17451145a5c58aaa7df2bf
sarayubacchu <42246042+sarayubacchu@users.noreply.github.com>
2018-08-17 10:59:47 +0100

    Update PracticeScript_SB.py

--- PracticeData/PracticeScript_SB.py
+++ PracticeData/PracticeScript_SB.py
@@ -3,17 +3,15 @@ from matplotlib import colors as mcolors
 import numpy as np
 
 x, y = np.loadtxt('2018-08-15T15_06_26-pupilproper.txt', delimiter=',', unpack=True)
-plt.plot(y,x)
+plt.plot(y,x, label='Trial1')
 
 x, y = np.loadtxt('pupilarea2.txt', delimiter=',', unpack=True,)
-plt.plot(y,x, color='green')
+plt.plot(y,x, color='green', label='Trial2')
 
 plt.xlabel('Time')
 plt.ylabel('Area of Pupil')
 plt.title('Area of Pupil Against Time')
 
-
+plt.legend()
 plt.savefig("GraphOneSB.png")
-
 plt.show()
-
